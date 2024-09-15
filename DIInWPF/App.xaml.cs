0ce6377f4a56d61da978e3dcaff41c691d563d87
Giedrius <kalniusg@gmail.com>
2024-09-15 15:31:48 +0300

    Git initial commit

---
+++ DIInWPF/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Windows;
+using WpfLibrary;
+using DIInWPF.StartupHelpers;
+
+namespace DIInWPF;
+
+public partial class App : Application
+{
+    // starting point for this app
+    public static IHost? AppHost { get; private set; }
+
+    public App()
+    {
+        AppHost = Host.CreateDefaultBuilder()
+            .ConfigureServices((context, services) =>
+            {
+                //for main window use singleton because you just want to create just one main window not few instances
+                services.AddSingleton<MainWindow>();
+                //services.AddTransient<ChildForm>();
+                services.AddFormFactory<ChildForm>();
+                services.AddTransient<IDataAccess, DataAccess>();
+            })
+            .Build();
+    }
+
+    protected override async void OnStartup(StartupEventArgs e)
+    {
+        // this can be null so add exclamation point so it ignores null
+        await AppHost!.StartAsync();
+
+        var startupForm = AppHost.Services.GetRequiredService<MainWindow>();
+        startupForm.Show();
+
+        base.OnStartup(e);
+    }
+
+    protected override async void OnExit(ExitEventArgs e)
+    {
+        // stopping app host when exiting application
+        await AppHost!.StopAsync();
+        base.OnExit(e);
+    }
+}
---
+++ DIInWPF/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using DIInWPF.StartupHelpers;
+using System.Windows;
+using WpfLibrary;
+
+namespace DIInWPF;
+
+public partial class MainWindow : Window
+{
+    public readonly IDataAccess _DataAccess;
+    private readonly IAbstractFactory<ChildForm> _factory;
+    //private readonly ChildForm _childForm;
+
+    public MainWindow(IDataAccess dataAccess, IAbstractFactory<ChildForm> factory) //ChildForm childForm)
+    {
+        InitializeComponent();
+        _DataAccess = dataAccess;
+        _factory = factory;
+        //_childForm = childForm;
+    }
+
+    private void getData_Click(object sender, RoutedEventArgs e)
+    {
+        // text field
+        data.Text = _DataAccess.GetData();
+    }
+
+    private void openChildForm_Click(object sender, RoutedEventArgs e)
+    {
+        //_childForm.Show();
+        _factory.Create().Show();
+    }
+}
